1bf348a717e56a4489769a0ebb5f3e4c16066606
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-08-01 23:51:27 -0400

    object, types, and values - #1

---
+++ basics_c++/object_types_variables/input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "Please enter your first name (followed by \"enter\"): ";
+    std::string first_name;
+    std::cin >> first_name;
+    std::cout << "Hello, " << first_name << "!\n";
+}
---
+++ basics_c++/object_types_variables/variables.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Please enter your first name and age\n";
+    std::string first_name ="???";
+    int age = -1;
+    std::cin >> first_name >> age;
+    std::cout << "Hello, " << first_name << " (age " << age << ")\n";
+}
