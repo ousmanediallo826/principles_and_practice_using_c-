bc6112d2a48a8f9d95d7f6ef2749180b809f3d8d
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-08-10 11:50:18 -0400

    part one errors

---
+++ basics_c++/errors/error.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int area(int width, int length){
+    int result = width * length;
+    return result;
+};
+int main () {
+    
+
+    int s1 = area(7,2);
+    std::cout << s1 << " is the area of s1 \n";
+
+}
