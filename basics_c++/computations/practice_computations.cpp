10934c4dfbfafc7252929c513692aa966e7e4aee
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-08-15 15:24:39 -0400

    computation

--- basics_c++/computations/practice_computations.cpp
+++ basics_c++/computations/practice_computations.cpp
@@ -7,7 +7,38 @@ void MultiplyMultiplies(std::vector<int>& numbers, int factor) {
         }
     }
     
+};
+
+void sanitizeData(std::vector<int>& data) {
+    for (auto& num : data) {
+        if (num < 0) {
+            num  = 0;
+        }
+    }
+};
+
+int countFrequency(const std::vector<std::string>& words, const std::string& target) {
+    int count = 0;
+
+    for (auto& word : words) {
+        if (word == target) {
+            count += 1;
+        }
+    }
+    return count;
 }
+
+int findEvenNumber(std::vector<int>& data) {
+    for(auto& num : data) {
+        if (num % 2 == 0) std::cout << num << " is an even number.";
+        else if (num % 2 != 0) std::cout << num << " is an odd number";
+        else if (num < 0 ) std::cout << "We don't compute negative number.";
+        else std::cout << "This is either not a integer";
+    }
+    return 0;
+}
+
+
 
 int main() {
     // std::vector<int> numbers = {1,2,3,4,5,6};
@@ -44,6 +75,26 @@ int main() {
         std::cout << num << " ";
     }
     std::cout << "\n";
+
+
+//    Exercise 2: Replace Negatives with Zero
+
+    std::vector<int> data = {10, -5, 3, -12, 0, 8};
+    sanitizeData(data);
+
+    for( int num : data) {
+        std::cout << num << " ";
+    };
+
+
+    std::vector<int> datas {10, 3, 5, 7, 6, 8, 20, -2, -4, "ousmane"};
+    findEvenNumber(datas);
+    for (int num : datas) std::cout <<" " << num << " ";
+
+
+
+
+
 
 
 
