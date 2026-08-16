6e491885b030e1ced47b44e6027ed6e055e9ea97
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-08-16 19:38:13 -0400

    first attempts of the calculator

---
+++ basics_c++/write_a_program/program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main () {
+    std::cout << "Please enter expression ( we can handle '+' and '-'):";
+    int lval = 0;
+    int rval = 0;
+    char op = 0;
+    int res = 0;
+    std::cin >> lval >> op >> rval;
+
+    if (op == '+') res = lval + rval;
+    else if (op == '-') res = lval - rval;
+    std::cout << "Result: " << res << "\n";
+}
