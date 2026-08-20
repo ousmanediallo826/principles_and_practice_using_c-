1d7b33333b3dfe7cefe85ed30fa3c0b8f9bf7cc7
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-08-20 13:05:57 -0400

    Technicalities functions Part two

--- basics_c++/technicalities/tech_functions.cpp
+++ basics_c++/technicalities/tech_functions.cpp
@@ -22,12 +22,13 @@ struct X {
             g(m+2.3); }
         }
         X() {} int m3() {
+            return 0;
 
         };
        
     };
 
-int my_find(std::vector<std::string> vs, std::string s, int hint) {
+int my_find(const std::vector<std::string>& vs, const std::string& s, int hint) {
     if (hint < 0 || vs.size() <= hint) hint = 0;
     for (int i = hint; i < vs.size(); ++i) {
         if (vs[i] == s) return i;
@@ -38,6 +39,30 @@ int my_find(std::vector<std::string> vs, std::string s, int hint) {
     }
     return -1;
 }
+
+// Pass-by const reference
+void print(const std::vector<double>& v) {
+    std::cout << "{";
+    for(int i = 0; i < v.size(); ++i) {
+        std::cout << v[i];
+        if (i!=v.size() -1) std::cout << ",";
+
+    }
+    std::cout << "} \n";
+}
+
+void f(int x) {
+    std::vector<double> vd1(10);
+    std::vector<double> vd2(1000000000);
+    std::vector<double> vd3(x);
+
+
+    print(vd1);
+    print(vd2);
+    print(vd3);
+}
+
+
 
 int main() {
     X a; a.f(2);
